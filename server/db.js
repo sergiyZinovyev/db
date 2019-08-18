72b3b2d3906e6f5ee8be49aa694c1bfa6285502c
sergiyZinovyev <thrion1@gmail.com>
2019-08-18 16:40:52 +0300

    db 0.4.3

--- server/db.js
+++ server/db.js
@@ -1,22 +1,23 @@
 const mysql = require("mysql2");
 
 // my db
-const connection = mysql.createPool({
-    connectionLimit: 100,
-    host: "remotemysql.com",
-    user: "bMDyIZAQkP",
-    database: "bMDyIZAQkP",
-    password: "OALl3svGQf"
-  });
+// const connection = mysql.createPool({
+//     connectionLimit: 100,
+//     host: "remotemysql.com",
+//     user: "bMDyIZAQkP",
+//     database: "bMDyIZAQkP",
+//     password: "OALl3svGQf"
+//   });
 
 //dev db
-// const connection = mysql.createPool({
-//   connectionLimit: 100,
-//   host: "192.168.5.105",
-//   user: "galexpo",
-//   database: "visitorsdev",
-//   password: "ufktrcgj"
-// });
+const connection = mysql.createPool({
+  connectionLimit: 100,
+  host: "192.168.5.105",
+  user: "galexpo",
+  database: "visitorsdev",
+  password: "ufktrcgj",
+  charset: "cp1251"
+});
 
 
 exports.get = function() {
