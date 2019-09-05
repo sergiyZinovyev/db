3319c0d5e44b026ba0706297ce2c7ea27097e670
sergiyZinovyev <thrion1@gmail.com>
2019-09-05 18:56:29 +0300

    db 0.4.23

--- src/app/shared/server.service.ts
+++ src/app/shared/server.service.ts
@@ -17,6 +17,7 @@ export class ServerService {
   frontURL: URL;
   apiUrl = 'http://localhost:7001'; //dev host
   //apiUrl = 'http://192.168.5.107:7001'; //prod host
+  //apiUrl = 'http://fort-op.galexpo.lviv.ua:7001'; //www host
 
   constructor(
     private http: HttpClient
